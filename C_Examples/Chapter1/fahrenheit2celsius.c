ac084cf8fb5fb4ca6841d10d2840c82bccecce15
root <root@DESKTOP-O2T9S6J.localdomain>
2021-09-09 11:01:20 -0600

    Added hello.md

--- C_Examples/Chapter1/fahrenheit2celsius.c
+++ C_Examples/Chapter1/fahrenheit2celsius.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
-	  for fahr = 0, 20, ..., 300 */
-int main() {
-	  int fahr, celsius;
-	  int lower, upper, step;
-	
-	  lower = 0;	/* lower limit of temperature table */
-	  upper = 300;	/* upper limit */
-	  step = 20;	/* step size */
+//converts fahrenheit into celsius
+float convert(float fahr){
+  float cel = (fahr -32)*5/9;
+  return cel;
+}
 
-	  fahr = lower;
-	  while (fahr <= upper) {
-		  celsius = 5 * (fahr-32) / 9;
-		  printf("%d\t%d\n", fahr, celsius);
-		  fahr = fahr + step;
-	  }
-	  return(0);
+int main(){
+	float fahr;
+    float cel;
+	//take user input as float
+    printf("Enter a temperature in Fahrenheit: ");
+    scanf("%f", &fahr);
+    cel = convert(fahr);
+    printf("\n %.2f Fahrenheit = %.2f Celsius", fahr, cel);
+	printf("\n");
 }
--- C_Examples/Chapter1/longest.c
+++ C_Examples/Chapter1/longest.c
@@ -5,8 +5,7 @@ int getaline(char line[], int maxline);
 void copy(char to[], char from[]);
 
 /* print longest input line */
-int main()
-{
+int main(){
     int len;              /* current line length */
     int max;              /* maximum length seen so far */
     char line[MAXLINE];   /* current input line */
@@ -14,13 +13,13 @@ int main()
 
     max = 0;
     while ((len = getaline(line, MAXLINE)) > 0)
-        if (len > max) {
-	    max = len;
-	    copy(longest, line);
-	}
-    if (max > 0) {    /* there was a line */
+        if (len > max){
+			max = len;
+			copy(longest, line);
+		}
+    if (max > 0){    /* there was a line */
         printf("%d\n", max);
-	printf("%s", longest);
+		printf("%s", longest);
     }
     return 0;
 }
@@ -30,19 +29,21 @@ int getaline(char s[], int lim)
 {
     int c, i;
 
-    for (i = 0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-	s[i] = c;
-    if (c == '\n') {
-	s[i] = c;
-	++i;
+    for (i = 0; (c=getchar())!=EOF && c!='\n'; ++i){
+		if(i < lim-1){
+			s[i] = c;
+		}
+	}
+    if (c == '\n'){
+		s[i] = c;
+		++i;
     }
     s[i] = '\0';
     return i;
 }
 
 /* copy:  copy 'from' into 'to'; assume to is big enough */
-void copy(char to[], char from[])
-{
+void copy(char to[], char from[]){
     int i;
 
     i = 0;
